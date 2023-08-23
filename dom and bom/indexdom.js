69d347989bf89bb28f02d74bfac03a5c34b39dbb
ADITYAPRASADSAHOOS1200 <apsahoo5555@gmail.com>
2023-08-24 02:05:24 +0530

    final commit

---
+++ dom and bom/indexdom.js
@@ -0,0 +1,20 @@
+let ctitle=document.getElementsByClassName("card-title")[0]
+ctitle.style.color="red";
+ctitle.style.fontSize="25px";
+
+let ctext=document.getElementById("card-text")
+ctext.style.color="blue"
+
+let ctitles=document.querySelectorAll(".card-title")[1]
+ctitles.style.color="blue"
+
+let dtitles=document.querySelectorAll(".card-title")[2]
+dtitles.style.color="violet"
+
+
+document.querySelector(".this").style.color="red"
+document.querySelector(".this").style.background="white"
+
+
+console.log(document.getElementsByTagName("a"))
+
