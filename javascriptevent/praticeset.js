268fabe4b2e3cfff5bc8a8cc38fa29b9b4178e43
ADITYAPRASADSAHOOS1200 <apsahoo5555@gmail.com>
2023-08-30 01:01:03 +0530

    final commit

--- javascriptevent/praticeset.js
+++ javascriptevent/praticeset.js
@@ -1,4 +1,26 @@
-document.getElementById("google").addEventListener("click",function(){
-    window.location="https://www.google.com";
-    
+window.document.getElementById("google").addEventListener("click",function(){
+   window.location="https://www.google.com";
+   
 })
+document.getElementById("facebook").addEventListener("click",function(){
+    window.location="https://www.facebook.com"
+})
+
+//question 3
+//fetch api
+
+
+let fetchContent=async(url)=>{
+    con=await fetch(url);
+    let a=await con.json();
+    return a;
+}
+setInterval(async function() {
+    let url='https://jsonplaceholder.typicode.com/todos/1'
+    console.log(await fetchContent(url));
+}, 3000);
+
+
+setInterval(async function(){
+ window.document.querySelector("#bulb").classList.toggle("bulb")
+}, 1000)
